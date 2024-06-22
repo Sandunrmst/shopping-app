13bcc2361089db95b25a90268131b4146ea35ab6
sandunrmst <sandunalugc@gmail.com>
2024-06-22 16:05:26 +0530

    use dispatch for handle addToCart and RemoveFromCart

--- src/components/add-to-cart-button/index.js
+++ src/components/add-to-cart-button/index.js
@@ -1,15 +1,33 @@
 "use client";
 
+import { removeFromCart } from "@/store/slices/cart-slice";
 import { Button } from "../ui/button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function AddToCartButton({ productItem }) {
   //get cart in state
   const { cart } = useSelector((state) => state);
 
+  const dispatch = useDispatch();
+
+  function handleAddToCart() {
+    dispatch(addToCart(productItem));
+  }
+
+  function handleRemoveFromCart() {
+    dispatch(removeFromCart(productItem?.id));
+  }
+
   return (
     <div className="mt-8 max-w-md">
-      <Button>
+      <Button
+        type="button"
+        onClick={cart?.cartItems.some(
+          ((item) => item.id === productItem.id)
+            ? handleRemoveFromCart
+            : handleAddToCart
+        )}
+      >
         {cart?.cartItems.some((item) => item.id === productItem.id)
           ? "Remove from cart"
           : "Add to cart"}
