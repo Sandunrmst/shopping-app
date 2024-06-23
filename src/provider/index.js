429ec380463d8807615bc960ac2389816ae03bd5
sandunrmst <sandunalugc@gmail.com>
2024-06-23 17:50:22 +0530

    get session to the header section

--- src/provider/index.js
+++ src/provider/index.js
@@ -4,10 +4,10 @@ import Headers from "@/components/header";
 import store from "@/store";
 import { Provider } from "react-redux";
 
-export default function ReduxProvider({ children }) {
+export default function ReduxProvider({ children, getSession }) {
   return (
     <Provider store={store}>
-      <Headers />
+      <Headers getSession={getSession} />
       {children}
     </Provider>
   );
